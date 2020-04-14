bb4f80a50635c109eae2971d741abea12424a775
NaveenJain808 <63161503+NaveenJain808@users.noreply.github.com>
2020-04-14 18:50:35 -0400

    Add files via upload

--- src/pa06/Cluster.java
+++ src/pa06/Cluster.java
@@ -1,20 +1,20 @@
-package pa06;
-
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.Scanner;
-
 public class Cluster {
-    
-    //accept an arraylist of samples and pick a random sample
-    public Cluster(ArrayList<Sample> list){
+    //Step 1 is to assign random cluster points
+    public Cluster(ArrayList<Sample> list, int numbercluster,ArrayList<Sample> clusters){
         //picks a random element in the array of samples to use as the cluster point
-        Random rand=new Random();
-        int index = rand.nextInt(list.size());
-        Sample clusterPoint=list.get(index);
-        //printing to verify that the clusterpoint and the random value agree
-        System.out.println("CP = " + clusterPoint);
-        System.out.println(index);
+        for(int i=0;i<numbercluster;i++){
+            Random rand=new Random();
+            int index = rand.nextInt(list.size());
+            Sample ClusterPoint=list.get(index);
+            clusters.add(ClusterPoint);
+        }
+        //test to see that clusters have been randomly chosen
+        for(int j=0;j<clusters.size();j++){
+            Sample x=clusters.get(j);
+            System.out.println(x.getvalue(0));
+            System.out.println(x.getvalue(1));
+        }
     }
-
 }
