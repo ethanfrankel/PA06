5c9a95930e66b57330aac6f17b82f1edcc0622c9
NaveenJain808 <63161503+NaveenJain808@users.noreply.github.com>
2020-04-14 18:51:09 -0400

    Add files via upload

--- src/pa06/Sample.java
+++ src/pa06/Sample.java
@@ -1,5 +1,3 @@
-package pa06;
-
 import java.util.ArrayList;
 import java.util.*;
 /**
@@ -10,12 +8,16 @@ import java.util.*;
 
 public class Sample {
 	ArrayList<Double> sample;
-	
+	//regular constructor for filling the arraylist of samples from file
 	public Sample(double[] values) {
 		this.sample = new ArrayList<Double>();
 		for (int i=0; i<values.length; i++) {
 			sample.add(values[i]);
 		}	
+	}
+	//default constructor if you want to use difference class
+	public Sample() {
+		
 	}
 	double getvalue(int i){
 		return sample.get(i);
@@ -23,26 +25,9 @@ public class Sample {
 	double getlength(){
 		return sample.size();
 	}
-	public static void main(String[] args) {
-		System.out.println("testing for the Sample class.");
-		//add samples
-		double[] p1 = {1d, 2d, 3.14, 2.71};
-		double[] p2 = {0.1, 1.1, 2.1, 3.1};
-		Sample s1 = new Sample(p1);
-		Sample s2 = new Sample(p2);
-		System.out.println("s1 = "+s1);
-		System.out.println("s2 = "+s2);
-		System.out.println(difference(s1, s2));
-		//create an arraylist of all samples
-		ArrayList<Sample> SampleList = new ArrayList<Sample>();
-		SampleList.add(s1);
-		SampleList.add(s2);
-		//call the cluster class to pick a cluster point
-		Cluster cluster=new Cluster(SampleList);
-	}
-
-	public static double difference(Sample s1, Sample s2) {
-		//variance of samples as described in part 1
+	
+	double difference(Sample s1, Sample s2) {
+		//difference between samples
 		double SumofSquare=0;
 		for(int i=0;i<s2.getlength();i++){
 			double sum=s1.getvalue(i)-s2.getvalue(i);
@@ -50,20 +35,5 @@ public class Sample {
 			SumofSquare=SumofSquare+sum;
 		}
 		return Math.sqrt(SumofSquare);
-	}
-	
-	public String toString() {
-		String toString = "[";
-	    for (int i = 0; i < this.sample.size(); i++) {
-	      //this if statement essentially does not print a comma after the last item in list
-	      if (i == this.sample.size() - 1) {
-	    	toString = toString + this.sample.get(i);
-	      }
-	      else {
-	    	toString = toString + this.sample.get(i) + ", ";
-	      }
-	    }
-	    toString = toString + "]";
-	    return toString;
 	}
 }
