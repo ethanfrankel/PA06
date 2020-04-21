2bc2186baca63816e81875c31779bb9012366d63
NaveenJain808 <63161503+NaveenJain808@users.noreply.github.com>
2020-04-20 23:11:08 -0400

    Add files via upload

--- src/pa06/Cluster.java
+++ src/pa06/Cluster.java
@@ -1,37 +1,18 @@
 package pa06;
 import java.util.ArrayList;
 import java.util.Random;
-/**
-* Cluster is a grouping of Samples that split the overall population into subdivisions 
-* in which all members or samples share similar characterisitcs, in this case distance 
-* or proximity to one another. 
-*/
 public class Cluster {
     //Step 1 is to assign random cluster points
     public Cluster(ArrayList<Sample> list, int numbercluster,ArrayList<Sample> clusters){
-        //A for loop to pick a random element in the array of samples to use as the cluster point
-         /**
-         *@param list is the overall data set made out of Samples
-         *@param numbercluster is the number of clusters there are
-         *@param clusters is another ArrayList but instead to hold the samples which belong to
-         *their respective cluster. 
-         */
-            for(int i=0;i<numbercluster;i++){
+        //picks a random element in the array of samples to use as the cluster point
+        for(int i=0;i<numbercluster;i++){
             Random rand=new Random();
             int index = rand.nextInt(list.size());
             System.out.println("Initial cluster "+(i+1)+"  at index "+index+" of original data");
             Sample ClusterPoint=list.get(index);
             clusters.add(ClusterPoint);
-          /**
-          * Each cluster will have a randomly assigned clusterPoint that will be the beginning
-          * basis for each. We add @param clusterPoint to @param clusters because it will be
-          * the first point that we can test distance to other points with. 
-          * @param index helps us generate a random number from 0 to however many elements
-          * there are in our overall list of Samples.
-          *
-          */
         }
-        //a test to see that clusters have been randomly chosen
+        //test to see that clusters have been randomly chosen
         for(int j=0;j<clusters.size();j++){
             Sample x=clusters.get(j);
             
--- src/pa06/Sample.java
+++ src/pa06/Sample.java
@@ -8,10 +8,6 @@ import java.util.*;
  */
 
 public class Sample {
-	/**
-	*@param sample is the Arraylist which stores the data (doubles) that we will be grouping into clusters.
-	*@loc is an int used for testing purposes that does not influence the class
-	*/
 	ArrayList<Double> sample;
 	int loc;
 	//regular constructor for filling the arraylist of samples from file
@@ -21,28 +17,24 @@ public class Sample {
 			sample.add(values[i]);
 		}	
 	}
-	//default constructor for sample class
+	//default constructor if you want to use difference class
 	public Sample() {
 		
 	}
-	//returns the value (double) associated with the appropriate sample 
-	public double getvalue(int i){
+	double getvalue(int i){
 		return sample.get(i);
 	}
-	//returns how many samples there are 
-	public double getlength(){
+	double getlength(){
 		return sample.size();
 	}
-	//used for testing purposes
-	public void setloc(Sample s1,int index){
+	void setloc(Sample s1,int index){
 		s1.loc=index;
 	}
-	//used for testing purpoes
-	public int  getloc(){
+	int  getloc(){
 		return this.loc;
 	}
-	//utilization of the Pythagorean Theorem to calculate distance, based on their associated doubles, between two Samples. 
-	public double difference(Sample s1, Sample s2) {
+	
+	double difference(Sample s1, Sample s2) {
 		//difference between samples
 		double x=s1.getvalue(0)-s2.getvalue(0);
 		x=x*x;
@@ -52,7 +44,6 @@ public class Sample {
 		z=Math.sqrt(z);
 		return z;
 	}
-	//String representation of a Sample
 	public String toString() {
 		String string = "(";
 	    for (int i = 0; i < this.sample.size(); i++) {
